655226c3da4ab1f0b1a582dc523805a47357f8bb
Aleksandar <aleksandar.zhivkovic@gmail.com>
2017-03-08 20:28:05 +0100

    Initial commit.

---
+++ app/src/main/java/upm/softwaredesign/uber/fragments/MapViewFragment.java
@@ -0,0 +1,45 @@
+package upm.softwaredesign.uber.fragments;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import upm.softwaredesign.uber.R;
+
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map_view, container, false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Madrid and move the camera
+        LatLng madrid = new LatLng(40.416775, -3.703790);
+        googleMap.addMarker(new MarkerOptions().position(madrid).title("Destination"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(madrid));
+    }
+}
